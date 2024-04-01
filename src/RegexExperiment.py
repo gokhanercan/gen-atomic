8d11fdeb3acd35081a8ae964c17281cff8f7bcea
gokhanercan <gokhanercan@gmail.com>
2024-04-01 11:48:11 +0300

    initial

---
+++ src/RegexExperiment.py
@@ -0,0 +1,88 @@
+
+# pip install langchain
+from abc import ABC, abstractmethod
+import re
+
+class Field(object):
+
+    def __init__(self, name:str,desc:str,correctCases,incorrectCases):
+        self.Name = name
+        self.Description = desc
+        self.CorrectCases:[str] = correctCases
+        self.IncorrectCases:[str] = incorrectCases
+
+    def __str__(self) -> str:
+        return  self.Name
+
+
+class IRegexGenerator(ABC):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    @abstractmethod
+    def Generate(self, description:str)->str:   #desc: price for TK
+        pass
+
+
+class StubRegexGenerator(IRegexGenerator):
+    def __init__(self, regex:str) -> None:
+        super().__init__()
+        self.Regex = regex
+
+    def Generate(self, description: str) -> str:
+        return self.Regex
+
+class RegexHost(object):        #TODO: Load ds here.
+
+    def __init__(self, regexGen:IRegexGenerator) -> None:
+        super().__init__()
+        self.RegexGen:IRegexGenerator = regexGen
+
+    def Run(self, fields):
+        # TODO: df here.
+        passedCaseCount:int = 0
+        totalCaseCount: int = 0
+        for f in fields:
+            generated:str = self.RegexGen.Generate(f.Description)
+            for cc in f.CorrectCases:
+                passed:bool = RegexHost.validate_regex(generated,cc)
+                if(passed): passedCaseCount = passedCaseCount + 1
+                totalCaseCount = totalCaseCount + 1
+                if(passed):
+                    print("[Passed] Field '" + str(f) + "':" + "with regex '" + generated + "' for correct case " + cc)
+                else:
+                    print("[Failed] Field '" + str(f) + "':" + "with regex '" + generated + "' for correct case " + cc)
+            #TODO: Incorrect cases
+
+        print("Overall accuracy:" + str((float(passedCaseCount) / float(totalCaseCount)) * 100))
+
+    @staticmethod
+    def validate_regex(regex_pattern, test_string)->bool:
+        try:
+            compiled_pattern = re.compile(regex_pattern)
+            match = compiled_pattern.search(test_string)
+            if match:
+                print("Regex is valid and matches the test string.")
+                return True
+            else:
+                print("Regex is valid but does not match the test string.")
+                return False
+        except re.error:
+            print("Invalid regular expression pattern.")
+            return False
+
+if __name__ == '__main__':
+    # from ds
+    fields = [
+        Field("Email","email address",
+              ["mail@gokhanercan.com","amojtehed@gmail.com"],
+              ["dsadsadasda","http://invalidaemail"]
+        ),
+        Field("PriceInTL","price formatted with thousands seperator in Turkish Lira currency",
+              ["1.550,5","100"],
+              ["090","0,23,34","12.11,23","aaaa","mail@gokhan.com"]
+        )
+    ]
+    fixedRegex:str = """^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"""
+    RegexHost(StubRegexGenerator(fixedRegex)).Run(fields)
