62013072990c3e6278a19fbce1fe5b54d22034ea
gokhanercan <gokhanercan@gmail.com>
2024-04-13 09:26:46 +0300

    New provider folders

--- src/models/OllamaModels.py
+++ src/models/OllamaModels.py
@@ -1,6 +1,9 @@
 from src.models.ModelBase import ModelBase
 
 class OllamaModels(ModelBase):
+    def __init__(self, modelName:str) -> None:
+        super().__init__()
+        self.ModelName = modelName
 
     def Generate(self, description: str) -> str:
-        raise Exception("not implemented.")
+        raise Exception("not implemented.") #pass modelName and run llama2
