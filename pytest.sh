de0aba9644a904783fcf48580617e8c1c025dc60
Gökhan Ercan <mail@gokhanercan.com>
2024-04-30 16:07:51 +0300

    Fix collect-only mode. Now tests actually running.

--- pytest.sh
+++ pytest.sh
@@ -2,5 +2,5 @@ SCRIPT_DIR=$(dirname "$0")
 echo "Script directory: $SCRIPT_DIR"
 path=$SCRIPT_DIR
 echo $path
-pytest $path --collect-only -v
+pytest $path -v
 read -rsp $'Press any key to continue...\n' -n1 key
